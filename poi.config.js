e379ae05fe36f1254d7d394d229d2f4c1ef533a0
Nick Landgrebe <nick@landgre.be>
2018-02-06 14:06:47 -0600

    Configure asset bundles for import pipeline

--- poi.config.js
+++ poi.config.js
@@ -1,12 +1,21 @@
+const pkg = require('./package.json');
+
 module.exports = {
   // Entry is relative to process.cwd()
   entry: [
     'index.jsx',
   ],
 
-  dist: 'dist',
+  dist: 'lib/assets',
 
   presets: [
     require('poi-preset-react')()
-  ]
+  ],
+
+  filename: {
+    js: 'javascripts/' + pkg.name + '-[name]_bundle.js',
+    css: 'stylesheets/' + pkg.name + '-[name]_bundle.css',
+    images: 'images/[name].[ext]',
+    fonts: 'fonts/[name].[ext]',
+  }
 };
