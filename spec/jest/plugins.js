511ba33869d4f91667dfcc8f6f5618b9f5a6f075
Nick Landgrebe <nick@landgre.be>
2018-02-17 23:02:25 -0600

    Improve jest plugins

---
+++ spec/jest/plugins.js
@@ -0,0 +1,12 @@
+require('jest-plugins')([
+  // List all jest-plugins here.
+  'jest-plugin-action',
+  'jest-plugin-context',
+  'jest-plugin-for-each',
+  'jest-plugin-it-renders',
+  'jest-plugin-its',
+  'jest-plugin-set',
+  'jest-plugin-unhandled-promise',
+]);
+
+require('jest-enzyme/lib');
