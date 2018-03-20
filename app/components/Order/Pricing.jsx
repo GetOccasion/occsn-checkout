373c9773148c12265878b57633942cecad5f4b2b
Nick Landgrebe <nick@landgre.be>
2018-03-20 15:47:52 -0400

    Add couponAmount to Pricing

--- app/components/Order/Pricing.jsx
+++ app/components/Order/Pricing.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Currency from 'react-currency-formatter';
 import Decimal from 'decimal.js';
+import _ from 'underscore';
 
 import { Card, CardBody, CardText } from 'reactstrap';
 
@@ -20,18 +21,35 @@ export default class Pricing extends PureComponent {
 
     let currency = order.product().merchant().currency().name;
 
+    var displaySubtotal = false;
+
     if(Decimal(order.taxPercentage).toNumber() > 0.0) {
-      rows.push(
-        <p className="subtotal">
-          <span>Subtotal: </span>
-          <Currency currency={currency} quantity={order.subtotal} />
-        </p>
-      );
+      displaySubtotal = true;
 
       rows.push(
         <p className="tax">
           <span>Tax ({ order.taxPercentage }%): </span>
           <Currency currency={currency} quantity={order.tax} />
+        </p>
+      );
+    }
+
+    if(!_.isNull(order.coupon())) {
+      displaySubtotal = true;
+
+      rows.push(
+        <p className="coupon">
+          <span>Coupon Discount: </span>
+          -<Currency currency={currency} quantity={order.couponAmount} />
+        </p>
+      );
+    }
+
+    if(displaySubtotal) {
+      rows.unshift(
+        <p className="subtotal">
+          <span>Subtotal: </span>
+          <Currency currency={currency} quantity={order.subtotal} />
         </p>
       );
     }
--- spec/components/Order/Pricing.spec.js
+++ spec/components/Order/Pricing.spec.js
@@ -8,24 +8,30 @@ import occsn from 'app/libs/Occasion';
 import productFixture from 'fixtures/products/cash.json';
 import productEUFixture from 'fixtures/products/cash_eur.json';
 import blankQuestionsFixture from 'fixtures/blank.json';
+import redeemableFixture from 'fixtures/products/redeemables/coupons/fixed.json';
 
 import initializedOrderPriceFixture from 'fixtures/orders/initialized/cash/price.json';
 import initializedOrderPriceTaxFixture from 'fixtures/orders/initialized/cash/price_tax.json';
+import initializedOrderCouponFixture from 'fixtures/orders/initialized/coupons/fixed.json';
 
 describe('Order', () => {
   describe('Pricing', () => {
     let order, product, wrapper;
 
-    async function setupWrapper(orderResponses) {
+    async function setupWrapper(customResponses, customProps) {
       let responses = {
         '/products/:id/': { status: 200, data: productFixture },
         '/products/:id/questions/': { status: 200, data: blankQuestionsFixture },
-        ...orderResponses
+        ...customResponses
       };
       axios._setMockResponses(responses);
 
-      product = await occsn.Product.find('1');
-      order = await occsn.Order.construct({ product, status: 'initialized' });
+      if(customProps == undefined) {
+        product = await occsn.Product.find('1');
+        order = await occsn.Order.construct({ product, status: 'initialized' });
+      } else {
+        await customProps();
+      }
 
       order = await order.save();
 
@@ -95,6 +101,31 @@ describe('Order', () => {
 
       it('displays price', () => {
         expect(wrapper.find('.total')).toIncludeText('$10.30')
+      });
+    });
+
+    context('when couponAmount', () => {
+      beforeEach(async () => {
+        await setupWrapper({
+          '/products/:id/redeemables/': { status: 200, data: redeemableFixture },
+          '/orders/': { status: 201, data: initializedOrderCouponFixture }
+        }, async () => {
+          product = await occsn.Product.find('1');
+          var coupon = await product.redeemables().findBy({ code: 'CODE' });
+          order = await occsn.Order.construct({ product, coupon, status: 'initialized' });
+        });
+      });
+
+      it('displays subtotal', () => {
+        expect(wrapper.find('.subtotal')).toIncludeText('$10.00')
+      });
+
+      it('displays coupon amount', () => {
+        expect(wrapper.find('.coupon')).toIncludeText('-$2.00')
+      });
+
+      it('displays price', () => {
+        expect(wrapper.find('.total')).toIncludeText('$8.00')
       });
     });
   });
