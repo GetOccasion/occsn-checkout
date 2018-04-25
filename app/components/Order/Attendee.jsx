5daaaecc596ec80193bd9ae24a1b5804568496da
Nick Landgrebe <nick@landgre.be>
2018-04-25 16:26:20 -0400

    Add Attendee component

---
+++ app/components/Order/Attendee.jsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import { Field } from 'mitragyna';
+
+import s from 'underscore.string';
+
+import occsn from '../../libs/Occasion';
+
+import { Card, CardBody, CardTitle, CardText, FormGroup, Input } from 'reactstrap';
+
+export default class Attendee extends PureComponent {
+  static propTypes = {
+    indexOf: PropTypes.number.isRequired,
+    questions: PropTypes.shape({
+      __collection: PropTypes.arrayOf(PropTypes.string)
+    }).isRequired,
+    subject: PropTypes.instanceOf(occsn.Attendee).isRequired,
+  };
+
+  render() {
+    let { indexOf, questions } = this.props;
+
+    debugger;
+
+    return <Card className="mb-2">
+      <CardBody>
+        <CardTitle>
+          Attendee { indexOf }
+        </CardTitle>
+        {
+          questions.map((q) => {
+            return <FormGroup>
+              <Field type='text' name={q} placeholder={s.humanize(q)} component={Input} invalidClassName='is-invalid'></Field>
+            </FormGroup>;
+          }).toArray()
+        }
+      </CardBody>
+    </Card>;
+  }
+}
---
+++ spec/components/Order/Attendee.spec.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Attendee from 'app/components/Order/Attendee';
+
+import axios from 'axios';
+import occsn from 'app/libs/Occasion';
+
+import productFixture from 'fixtures/products/attendee_questions.json';
+
+describe('Order', () => {
+  describe('Attendee', () => {
+    let attendee, product, wrapper;
+
+    async function setupWrapper(customResponses, customProps) {
+      let responses = {
+        '/products/:id/': { status: 200, data: productFixture },
+        ...customResponses
+      };
+      axios._setMockResponses(responses);
+
+      product = await occsn.Product.find('1');
+      attendee = occsn.Order.build().attendees().build();
+
+      let props = {
+        indexOf: 1,
+        questions: product.attendeeQuestions,
+        subject: attendee,
+        ...customProps,
+      };
+
+      let context = {
+        resource: attendee
+      };
+
+      let childContextTypes = {
+        resource: PropTypes.object
+      };
+
+      wrapper = shallow(<Attendee {...props} />, { context, childContextTypes });
+    }
+
+    beforeEach(async () => {
+      await setupWrapper({}, {});
+    });
+
+    afterEach(async () => {
+      axios.reset();
+    });
+
+    it('matches snapshot', () => {
+      expect(wrapper.find('Card')).toMatchSnapshot();
+    });
+  });
+});
