4d7b3f57ad3fd649bba5cac9af3087e4561ac1c7
Pelle ten Cate <pelle@technoot.nl>
2018-12-17 17:03:09 -0600

    Remove AR dependency

--- poi.config.js
+++ poi.config.js
@@ -13,12 +13,7 @@ module.exports = {
   },
 
   plugins: [
-    require('poi-plugin-react')(),
-    require('poi-plugin-resolve-alias')({
-      'active-resource': path.join(process.cwd(), 'node_modules', 'active-resource', 'build', 'active-resource.js'),
-      'mitragyna': path.join(process.cwd(), 'node_modules', 'mitragyna', 'build', 'mitragyna.min.js'),
-      'occasion-sdk': path.join(process.cwd(), 'node_modules', 'occasion-sdk', 'build', 'occasion-sdk.js'),
-    })
+    require('poi-plugin-react')()
   ],
 
   configureWebpack(config) {
