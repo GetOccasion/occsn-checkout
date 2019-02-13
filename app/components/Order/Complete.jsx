ccf47e62da104a5a3f34377340b40029d082d86d
Kieran Klaassen <kieranklaassen@gmail.com>
2019-02-13 13:00:25 -0600

    Make sure to render html for compelte message
    Fixes https://github.com/GetOccasion/occasion/issues/8540

--- app/components/Order/Complete.jsx
+++ app/components/Order/Complete.jsx
@@ -1,35 +1,43 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import ReactHtmlParser from 'react-html-parser'
 
-import { Jumbotron } from 'reactstrap';
+import { Jumbotron } from 'reactstrap'
 
-import occsn from '../../libs/Occasion';
+import occsn from '../../libs/Occasion'
 
 export default class OrderComplete extends PureComponent {
   static propTypes = {
-    order: PropTypes.instanceOf(occsn.Order),
-  };
+    order: PropTypes.instanceOf(occsn.Order)
+  }
 
   render() {
-    let { order } = this.props;
+    let { order } = this.props
 
-    return <section className="order-complete-container">
-      <h1 className="verification-code">Order #{ order.verificationCode }</h1>
-      {
-        !order.timeSlots().empty() ? (
+    return (
+      <section className="order-complete-container">
+        <h1 className="verification-code">Order #{order.verificationCode}</h1>
+        {!order.timeSlots().empty() ? (
           <section className="order-complete-time-slot-details">
-            {
-              order.timeSlots().target().map((timeSlot) => {
-                return <h4>{ timeSlot.toString('LLLL') }</h4>
-              }).toArray()
-            }
+            {order
+              .timeSlots()
+              .target()
+              .map(timeSlot => {
+                return <h4>{timeSlot.toString('LLLL')}</h4>
+              })
+              .toArray()}
           </section>
-        ) : (null)
-      }
-      <section className="order-complete-messages">
-        <p className="custom-confirmation-message">{ order.product().customerConfirmationMessage }</p>
-        <p className="confirmation-email-message">An order confirmation email with receipt has been sent to { order.customer().email }.</p>
+        ) : null}
+        <section className="order-complete-messages">
+          <p className="custom-confirmation-message">
+            {ReactHtmlParser(order.product().customerConfirmationMessage)}
+          </p>
+          <p className="confirmation-email-message">
+            An order confirmation email with receipt has been sent to{' '}
+            {order.customer().email}.
+          </p>
+        </section>
       </section>
-    </section>;
+    )
   }
 }
